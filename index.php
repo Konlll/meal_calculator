a56a2e3ba262cefabd04e0230c80e755663f82d9
Konlll <hungarokornel@gmail.com>
2018-10-16 20:15:45 +0200

    Bootstrap és a style.css file importálása.

--- index.php
+++ index.php
@@ -4,9 +4,14 @@
 		<title>Bejelentkezés</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=0, shrink-to-fit=no">
+		<!-- CSS files -->
 		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
 	</head>
 	<body>
+		
 
+		<!-- Script files -->
+		<script src="lib/bootstrap/js/bootstrap.min.js"></script>
 	</body>
 </html>
