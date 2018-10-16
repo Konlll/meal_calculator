fe08b3671c74cb22c98c12236276bba3a38835dd
Konlll <hungarokornel@gmail.com>
2018-10-16 20:04:07 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Bejelentkezés</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=0, shrink-to-fit=no">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body>
+
+	</body>
+</html>
