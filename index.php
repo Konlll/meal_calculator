7c58a7b0fcd4630c912bd831be4f4d6667c17100
Konlll <hungarokornel@gmail.com>
2018-10-20 20:06:20 +0200

    Logo megcsinálása Logoból favicon csinálása A bejelentkezés input megcsinálása

--- index.php
+++ index.php
@@ -4,12 +4,24 @@
 		<title>Bejelentkezés</title>
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=0, shrink-to-fit=no">
+		<link rel="shortcut icon" href="logo/favicon.ico" type="image/x-icon">
 		<!-- CSS files -->
-		<link rel="stylesheet" type="text/css" href="css/style.css">
+		<link rel="stylesheet" type="text/css" href="css/signin.css">
 		<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
 	</head>
 	<body>
-		
+		<!-- Bejelentkezés mező -->
+		<form class="form-signin text-center">
+			<img class="mb-4" src="logo/logo.png" alt="" width="200" height="200">
+			<h1 class="h3 mb-3 font-weight-normal">Kérlek jelentkezz be!</h1>
+			<label for="inputEmail" class="sr-only">E-mail cím</label>
+			<input type="email" id="inputEmail" class="form-control" placeholder="E-mail cím" required autofocus>
+			<label for="inputPassword" class="sr-only">Jelszó</label>
+			<input type="password" id="inputPassword" class="form-control" placeholder="Jelszó" required>
+			<button class="btn btn-lg btn-primary btn-block" type="submit">Bejelentkezés</button>
+			<p class="mt-5 mb-3 text-muted">&copy; Meal Calculator, 2018</p>
+		</form>
+
 
 		<!-- Script files -->
 		<script src="lib/bootstrap/js/bootstrap.min.js"></script>
