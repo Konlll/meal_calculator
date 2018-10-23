dce290261148b0f96cac42d1f74c041360a9b880
Konlll <hungarokornel@gmail.com>
2018-10-23 12:37:23 +0200

    Bejelentkezés megcsinálása home.php-ba kiírni a bejelentkezett felhasználót

---
+++ home.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Bejelentkezés</title>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="shortcut icon" href="logo/favicon.ico" type="image/x-icon">
+		<!-- CSS files -->
+		<link rel="stylesheet" href="lib/bootstrap/css/bootstrap.min.css">
+	</head>
+	<body>
+		<div class="container-fluid">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                    <a class="navbar-brand" href="#">Meal Calculator</a>
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Link</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" href="#">Disabled</a>
+                        </li>
+                    </ul>
+                    <div class="float-right mr-5">
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <?php
+                                    session_start();
+                                    echo "Üdv: {$_SESSION['username']}";
+                                ?>
+                            </button>
+                            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                <a class="dropdown-item text-center" href="#">Kijelentkezés</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+
+
+		<!-- Script files -->
+        <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+		<script src="lib/bootstrap/js/bootstrap.min.js"></script>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -1,4 +1,42 @@
-<!DOCTYPE html>
+<?php
+	if (isset($_POST['submit'])) {
+		$errors = array();
+		$true = true;
+
+		$email = strip_tags(trim($_POST['email']));
+		$password = strip_tags(trim($_POST['password']));
+
+		if (empty($email)) {
+			$true = false;
+			array_push($errors, "Nincs megadva e-mail cím!");
+		}
+		if (empty($password)) {
+			$true = false;
+			array_push($errors, "Nincs megadva jelszó!");
+		}
+
+		if ($true) {
+			$db = new mysqli("localhost","Konlll","Kornel2005","meal_calculator");
+
+			$password = sha1($password);
+			$sql = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+			$query = $db->query($sql);
+
+			if (mysqli_num_rows($query) == 1) {
+				while ($sor = mysqli_fetch_assoc($query)) {
+					session_start();
+					$_SESSION['belepve'] = "<div class='alert alert-success' role='alert'>Sikeres bejelentkezés!</div>";
+					$_SESSION['username'] = $sor['username'];
+					header('location: home.php');
+				}
+			}else{
+				array_push($errors, "Hibás adatok lettek megadva!");
+			}
+
+			$db->close();
+		}
+	}
+?><!DOCTYPE html>
 <html>
 	<head>
 		<title>Bejelentkezés</title>
@@ -11,26 +49,32 @@
 	</head>
 	<body>
 		<!-- Bejelentkezés mező -->
-		<form class="form-signin text-center">
+		<form class="form-signin text-center" method="POST">
 
 			<?php
 				// Sikeres regisztráció esetén
 				session_start();
-				if (!empty($_SESSION['registered'])) {
+				if (isset($_SESSION['registered'])) {
 					echo $_SESSION['registered'];
+				}
+
+				if (!empty($errors)) {
+					foreach ($errors as $error) {
+						echo "<div class='alert alert-info' role='alert'>{$error}</div>";
+					}
 				}
 			?>
 
 			<img class="mb-4" src="logo/logo.jpg" alt="" width="200" height="200">
-			<h1 class="h3 mb-3 font-weight-normal">Kérlek jelentkezz be!</h1>
+			<h1 class="h3 mb-3 font-weight-normal" style="text-shadow: 2px 2px grey;">Kérlek jelentkezz be!</h1>
 
-			<label for="inputEmail" class="sr-only">E-mail cím</label>
-			<input type="email" id="inputEmail" class="form-control" placeholder="E-mail cím" required autofocus>
+			<label for="email" class="sr-only">E-mail cím</label>
+			<input type="email" id="email" name="email" class="form-control" placeholder="E-mail cím" autofocus>
 			<br>
-			<label for="inputPassword" class="sr-only">Jelszó</label>
-			<input type="password" id="inputPassword" class="form-control" placeholder="Jelszó" required>
+			<label for="password" class="sr-only">Jelszó</label>
+			<input type="password" id="password" name="password" class="form-control" placeholder="Jelszó">
 			<br>
-			<button class="btn btn-lg btn-primary btn-block" type="submit">Bejelentkezés</button>
+			<button class="btn btn-lg btn-primary btn-block" type="submit" id="submit" name="submit">Bejelentkezés</button>
 
 			<p class="mt-4">Még nem regisztráltál? Itt megteheted: <a href="register.php">Regisztráció</a></p>
 
